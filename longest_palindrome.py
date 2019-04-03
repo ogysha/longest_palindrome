f739977b3ba4ce375ac58743793f0d76887277bf
Ognjen Stanic <ogysha@gmail.com>
2019-04-03 22:53:49 +0200

    Fixed length of substrings to consume

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -9,7 +9,7 @@ def find_longest_palindrome(string):
     longest = str()
 
     for start_idx in range(0, len(string)):
-        for end_idx in range(start_idx + 1, len(string)):
+        for end_idx in range(start_idx + 1, len(string) + 1):
             sub_string = string[start_idx:end_idx]
 
             if is_palindrome(sub_string) and len(sub_string) > len(longest):
@@ -19,8 +19,8 @@ def find_longest_palindrome(string):
 
 
 # Example:
-# $ python longest_palindrome.py "blanavolimilovanahablahabla"
-# anavolimilovana
+# $ python longest_palindrome.py "raaaaaaabcbaaaaaaar blanavolimilovanahablahabla raaaaaaacaaaaaaar"
+# raaaaaaabcbaaaaaaar
 
 def main(argv):
     check_input_arg(argv)
